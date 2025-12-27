6bee0fcb724c8de3d96d291e0eed754446c0dc23
Kseniya <saidova.kseniya@mail.ru>
2025-12-27 20:29:07 +0300

    Настроим админку для удобства services/admin.py

--- services/admin.py
+++ services/admin.py
@@ -1,6 +1,31 @@
 from django.contrib import admin
-from .models import Service, ServiceCategory
+from .models import ServiceCategory, Service
 
-# Простая регистрация сначала
-admin.site.register(ServiceCategory)
-admin.site.register(Service)
+
+@admin.register(ServiceCategory)
+class ServiceCategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'icon')
+    search_fields = ('name',)
+    list_per_page = 20
+
+
+@admin.register(Service)
+class ServiceAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category', 'price', 'duration', 'is_active')
+    list_filter = ('category', 'is_active')
+    search_fields = ('name', 'description')
+    list_editable = ('price', 'is_active')
+    list_per_page = 20
+    prepopulated_fields = {'slug': ('name',)}  # Автоматическое создание slug
+
+    fieldsets = (
+        ('Основная информация', {
+            'fields': ('name', 'slug', 'category', 'description', 'full_description')
+        }),
+        ('Детали', {
+            'fields': ('price', 'duration', 'image')
+        }),
+        ('Дополнительно', {
+            'fields': ('preparation', 'contraindications', 'is_active')
+        }),
+    )
