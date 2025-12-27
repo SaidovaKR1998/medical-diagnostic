3ca2954efa41f98b844932cd2717de3a111389b5
Kseniya <saidova.kseniya@mail.ru>
2025-12-27 19:02:25 +0300

    добавляем URL для аутентификации config/urls.py

--- config/urls.py
+++ config/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
     path('services/', include('services.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
 
 if settings.DEBUG:
