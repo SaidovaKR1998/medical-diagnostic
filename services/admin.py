a833475af111045fd3bc7e689df4de4cdcff5877
Kseniya <saidova.kseniya@mail.ru>
2025-12-27 18:32:52 +0300

    регистрируем модели в админке services/admin.py

--- services/admin.py
+++ services/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Service, ServiceCategory
 
-# Register your models here.
+@admin.register(ServiceCategory)
+class ServiceCategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'icon']
+    search_fields = ['name']
+
+@admin.register(Service)
+class ServiceAdmin(admin.ModelAdmin):
+    list_display = ['name', 'category', 'price', 'duration', 'is_active']
+    list_filter = ['category', 'is_active']
+    search_fields = ['name', 'description']
