25312574ee10290005f49f7c951af813207b2dfe
Kseniya <saidova.kseniya@mail.ru>
2025-12-27 18:37:20 +0300

    создаем services/admin.py (для начала простая регистрация)

--- services/admin.py
+++ services/admin.py
@@ -1,13 +1,6 @@
 from django.contrib import admin
 from .models import Service, ServiceCategory
 
-@admin.register(ServiceCategory)
-class ServiceCategoryAdmin(admin.ModelAdmin):
-    list_display = ['name', 'icon']
-    search_fields = ['name']
-
-@admin.register(Service)
-class ServiceAdmin(admin.ModelAdmin):
-    list_display = ['name', 'category', 'price', 'duration', 'is_active']
-    list_filter = ['category', 'is_active']
-    search_fields = ['name', 'description']
+# Простая регистрация сначала
+admin.site.register(ServiceCategory)
+admin.site.register(Service)
