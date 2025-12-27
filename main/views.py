8fa454c40d1070c18dda63d3bae30a8d682698f1
Kseniya <saidova.kseniya@mail.ru>
2025-12-27 19:03:10 +0300

    обновляем views.py для главной страницы main/views.py

--- main/views.py
+++ main/views.py
@@ -1,10 +1,22 @@
 from django.shortcuts import render
 
 def home(request):
-    return render(request, 'main/home.html')
+    """Главная страница"""
+    context = {
+        'title': 'Медицинская Диагностика - Главная',
+        'services': [
+            {'name': 'МРТ', 'icon': 'bi-magnet', 'description': 'Магнитно-резонансная томография'},
+            {'name': 'КТ', 'icon': 'bi-cpu', 'description': 'Компьютерная томография'},
+            {'name': 'УЗИ', 'icon': 'bi-soundwave', 'description': 'Ультразвуковая диагностика'},
+            {'name': 'Анализы', 'icon': 'bi-droplet', 'description': 'Лабораторные исследования'},
+        ]
+    }
+    return render(request, 'main/home.html', context)
 
 def about(request):
-    return render(request, 'main/about.html')
+    """Страница о компании"""
+    return render(request, 'main/about.html', {'title': 'О компании'})
 
 def contacts(request):
-    return render(request, 'main/contacts.html')
+    """Страница контактов"""
+    return render(request, 'main/contacts.html', {'title': 'Контакты'})
