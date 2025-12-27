a32b3118caa2702f8b6cfeeab8fd2aa12aaf2f6d
Kseniya <saidova.kseniya@mail.ru>
2025-12-27 20:39:02 +0300

    Доработка services/urls.py

--- services/urls.py
+++ services/urls.py
@@ -1,9 +1,16 @@
-from django.urls import path
 from . import views
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .api_views import ServiceCategoryViewSet, ServiceViewSet
 
 app_name = 'services'
+
+router = DefaultRouter()
+router.register(r'api/categories', ServiceCategoryViewSet)
+router.register(r'api/services', ServiceViewSet)
 
 urlpatterns = [
     path('', views.service_list, name='list'),
     path('<slug:slug>/', views.service_detail, name='detail'),
+    path('api/', include(router.urls)),
 ]
