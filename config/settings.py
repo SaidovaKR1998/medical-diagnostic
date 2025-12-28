c953b45a324f9f52dcd4e460c54f9e2b94f25c6e
Kseniya <saidova.kseniya@mail.ru>
2025-12-28 14:38:10 +0300

    Исправляем предупреждение о DEFAULT_AUTO_FIELD

--- config/settings.py
+++ config/settings.py
@@ -165,3 +165,7 @@ DATE_FORMAT = 'd.m.Y'
 DATETIME_FORMAT = 'd.m.Y H:i'
 SHORT_DATE_FORMAT = 'd.m.Y'
 SHORT_DATETIME_FORMAT = 'd.m.Y H:i'
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
