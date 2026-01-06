375937ba7b9c0ee60eb4bc0d1a8420daf518a131
Kseniya <saidova.kseniya@mail.ru>
2026-01-06 21:15:13 +0300

    Обновляем main/views.py так как добавили форму обратной связи main/forms.py

--- main/views.py
+++ main/views.py
@@ -1,7 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .forms import ContactForm
+
 
 def home(request):
     """Главная страница"""
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            # Здесь можно отправить email или сохранить в БД
+            messages.success(request, 'Спасибо за обращение! Мы свяжемся с вами в ближайшее время.')
+            return redirect('home')
+    else:
+        form = ContactForm()
+
     context = {
         'title': 'Медицинская Диагностика - Главная',
         'services': [
@@ -9,14 +21,23 @@ def home(request):
             {'name': 'КТ', 'icon': 'bi-cpu', 'description': 'Компьютерная томография высокой точности'},
             {'name': 'УЗИ', 'icon': 'bi-soundwave', 'description': 'Ультразвуковая диагностика'},
             {'name': 'Анализы', 'icon': 'bi-droplet', 'description': 'Лабораторные исследования'},
-        ]
+        ],
+        'form': form
     }
     return render(request, 'main/home.html', context)
 
-def about(request):
-    """Страница о компании"""
-    return render(request, 'main/about.html', {'title': 'О компании'})
 
 def contacts(request):
     """Страница контактов"""
-    return render(request, 'main/contacts.html', {'title': 'Контакты'})
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            messages.success(request, 'Сообщение отправлено! Мы ответим вам в течение 24 часов.')
+            return redirect('contacts')
+    else:
+        form = ContactForm()
+
+    return render(request, 'main/contacts.html', {
+        'title': 'Контакты',
+        'form': form
+    })
