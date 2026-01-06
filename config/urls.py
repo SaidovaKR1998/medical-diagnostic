62b6b259362594763e310d8948a1b96a24cd44ca
Kseniya <saidova.kseniya@mail.ru>
2026-01-06 19:25:51 +0300

    Настраиваем URLы. Обновляем config/urls.py

--- config/urls.py
+++ config/urls.py
@@ -7,8 +7,10 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
     path('services/', include('services.urls')),
-    path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/', include('accounts.urls')),  # Изменено с django.contrib.auth.urls
+    path('appointments/', include('appointments.urls')),
 ]
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
