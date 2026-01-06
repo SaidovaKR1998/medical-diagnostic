454d10f0974de925384f5d8f608079d2295a5012
Kseniya <saidova.kseniya@mail.ru>
2026-01-06 20:04:15 +0300

    Создаем базовые представления в appointments/views.py

--- appointments/views.py
+++ appointments/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from .models import Appointment
 
-# Create your views here.
+@login_required
+def appointment_list(request):
+    """Список записей"""
+    return render(request, 'appointments/list.html', {
+        'title': 'Мои записи'
+    })
+
+@login_required
+def create_appointment(request):
+    """Создание новой записи"""
+    return render(request, 'appointments/create.html', {
+        'title': 'Запись на прием'
+    })
+
+@login_required
+def appointment_detail(request, pk):
+    """Детали записи"""
+    return render(request, 'appointments/detail.html', {
+        'title': 'Детали записи'
+    })
