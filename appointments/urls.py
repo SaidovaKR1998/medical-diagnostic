401f08e07fab1cdf7f7c7a7bcaec0f19e1f2abe5
Kseniya <saidova.kseniya@mail.ru>
2026-01-06 21:05:56 +0300

    Исправить URL для страницы записи

--- appointments/urls.py
+++ appointments/urls.py
@@ -4,8 +4,8 @@ from . import views
 app_name = 'appointments'
 
 urlpatterns = [
-    # Пока оставляем пустым или добавляем базовые URL
     path('', views.appointment_list, name='list'),
     path('create/', views.create_appointment, name='create'),
     path('<int:pk>/', views.appointment_detail, name='detail'),
+    path('<int:pk>/cancel/', views.cancel_appointment, name='cancel'),
 ]
