0a3cccadc46ea0010d5acd08998a62cd9e088172
Kseniya <saidova.kseniya@mail.ru>
2026-01-06 19:21:12 +0300

    Обновляем config/settings.py Добавляем в самое начало: AUTH_USER_MODEL = 'accounts.CustomUser'

--- config/settings.py
+++ config/settings.py
@@ -5,9 +5,10 @@ from dotenv import load_dotenv  # Добавляем эту строку
 # Загружаем переменные окружения из .env файла
 load_dotenv()
 
+AUTH_USER_MODEL = 'accounts.CustomUser'
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
